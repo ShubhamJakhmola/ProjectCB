9a0285fe9e6ca292f907e595ac0cd8db2d97af52
Shubham_Jakhmola <82331070+ShubhamJakhmola@users.noreply.github.com>
2023-05-15 21:08:13 +0530

    Update wsgi.py

--- ecommerce/wsgi.py
+++ ecommerce/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import Settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce.settings')
+
 
 application = get_wsgi_application()
